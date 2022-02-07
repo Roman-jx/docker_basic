c10406ac2c0a8d6c101dec85ca8800511001eb07
Roman Malkevich <pomannero52@gmail.com>
2022-02-07 02:00:14 +0200

    v1

--- file1.php
+++ file1.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	<h1>
-	<?php
-$n = $i = $argv[1] ?? 5;
-while ($i--){
-	echo str_repeat(' ', $i).str_repeat("* ", $n - $i)."\n";
-}
-	</h1>
-</body>
-</html>
+<?php
+echo "Hello world!";
+
